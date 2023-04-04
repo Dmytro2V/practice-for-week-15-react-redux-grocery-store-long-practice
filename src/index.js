1f2435256d4866062ec6900db1e7b613817c7124
Dmytro2V <op2vin@gmail.com>
2023-04-04 11:24:36 +0300

    setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 import './index.css';
 import App from './App';
 
+const store = configureStore();
+// for testing only
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
 function Root() {
   return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   );
 }
 
